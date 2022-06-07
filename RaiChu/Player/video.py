f18aea7c619d09dc98dab909fd67b1a51a65bfa6
BHATAKTI ATMA <105307216+Bhataktiop@users.noreply.github.com>
2022-06-07 13:06:49 +0530

    Update video.py

--- RaiChu/Player/video.py
+++ RaiChu/Player/video.py
@@ -21,7 +21,7 @@ from pytgcalls.types.input_stream.quality import (
 from youtubesearchpython import VideosSearch
 from Process.design.thumbnail import play_thumb, queue_thumb
 
-IMAGE_THUMBNAIL = "https://telegra.ph/file/519b6bc739756cb822039.png"
+IMAGE_THUMBNAIL = "https://telegra.ph/file/bc7ba397bb66523469e62.jpg"
 
 def ytsearch(query: str):
     try:
@@ -183,8 +183,8 @@ async def vplay(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/BotDuniyaXd"),
-                            InlineKeyboardButton("💭 Support", url=f"https://t.me/godzilla_chatting")
+                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/Ishq_wala_lov3e"),
+                            InlineKeyboardButton("💭 Support", url=f"https://t.me/BHATAKTI_EMPIRE")
                         ],
                         [
                             InlineKeyboardButton("🗑 Close", callback_data="cls")
@@ -265,8 +265,8 @@ async def vplay(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/BotDuniyaXd"),
-                            InlineKeyboardButton("💭 Support", url=f"https://t.me/godzilla_chatting")
+                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/Ishq_wala_lov3e"),
+                            InlineKeyboardButton("💭 Support", url=f"https://t.me/BHATAKTI_EMPIRE")
                         ],
                         [
                             InlineKeyboardButton("🗑 Close", callback_data="cls")
